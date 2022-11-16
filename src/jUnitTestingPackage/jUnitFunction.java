b6016aa7858636f225d9e648715229e9c93e0ced
HARISHWA D REDDY <harishwadreddy@gmail.com>
2022-11-16 17:04:49 +0530

    jUnitTesting

--- src/jUnitTestingPackage/jUnitFunction.java
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -1,5 +1,9 @@
-package jUnitTestingPackage;
-
+package jUnitTestingPackage; 
 public class jUnitFunction {
-
+    public int addNumbers(int numl, int num2){ 
+        return numl+num2;
+    }
+    public String addStrings(String s1, String s2){
+        return s1+s2; 
+    }
 }
